a11d7474b6b7ddcdbb17dbe06118826e51bc33cf
Mu Ye <emailyemu@gmail.com>
2025-05-24 13:25:49 -0700

    Task 2: first attempt

--- src/kvsrv1/lock/lock.go
+++ src/kvsrv1/lock/lock.go
@@ -1,7 +1,10 @@
 package lock
 
 import (
-	"6.5840/kvtest1"
+	"sync"
+
+	"6.5840/kvsrv1/rpc"
+	kvtest "6.5840/kvtest1"
 )
 
 type Lock struct {
@@ -10,7 +13,13 @@ type Lock struct {
 	// Put and Get.  The tester passes the clerk in when calling
 	// MakeLock().
 	ck kvtest.IKVClerk
-	// You may add code here
+
+	mu sync.Mutex
+
+	// Lock state
+	owner string
+	key   string
+	done  chan bool
 }
 
 // The tester calls MakeLock() and passes in a k/v clerk; your code can
@@ -19,15 +28,42 @@ type Lock struct {
 // Use l as the key to store the "lock state" (you would have to decide
 // precisely what the lock state is).
 func MakeLock(ck kvtest.IKVClerk, l string) *Lock {
-	lk := &Lock{ck: ck}
-	// You may add code here
+	id := kvtest.RandValue(8)
+	lk := &Lock{ck: ck, owner: id, key: l}
 	return lk
 }
 
 func (lk *Lock) Acquire() {
-	// Your code here
+	lk.mu.Lock()
+	defer lk.mu.Unlock()
+
+	val, ver, err := lk.ck.Get(lk.key)
+	if err == rpc.ErrNoKey {
+		lk.ck.Put(lk.key, lk.owner, 0)
+		return
+	}
+	if val == "" {
+		lk.ck.Put(lk.key, lk.owner, ver)
+		return
+	}
+
+	<-lk.done
+	lk.ck.Put(lk.key, lk.owner, ver)
 }
 
 func (lk *Lock) Release() {
-	// Your code here
+	lk.mu.Lock()
+	defer lk.mu.Unlock()
+
+	val, ver, err := lk.ck.Get(lk.key)
+	if err == rpc.ErrNoKey || val == "" {
+		panic("cannot release an empty lock")
+	}
+	if val != lk.owner {
+		panic("cannot release someone else's lock")
+	}
+	lk.ck.Put(lk.key, "", ver)
+
+	// Does this work in a distributed env? Channels are in-memory
+	lk.done <- true
 }
